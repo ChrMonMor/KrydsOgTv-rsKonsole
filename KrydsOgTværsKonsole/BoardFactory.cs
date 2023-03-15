c891debf5d7befb96514d2218b51670ab327c9a2
ChrMonMor <97163196+ChrMonMor@users.noreply.github.com>
2023-03-15 15:22:04 +0100

    ved ikke hvad jeg elser skal lave til min opgave, så det er bare nogle for sjov ting jeg har lavet i dag

--- KrydsOgTværsKonsole/BoardFactory.cs
+++ KrydsOgTværsKonsole/BoardFactory.cs
@@ -29,28 +29,129 @@ namespace KrydsOgTværsKonsole
         }
         public void StartBoardGeneration(string startingLongWord)
         {
+            this.Board[1, 1] = '@';
             if (this.SizeX-2 < startingLongWord.Length)
             {
                 return;
             }
             int differnt = this.SizeX - startingLongWord.Length;
+            if (startingLongWord.Length % 2 == 0)
+            {
+                differnt++;
+            }
             int k = 0;
             for (int i = 0; i < this.SizeX; i++)
             {
-                if (i > differnt/2 || i < this.SizeX - differnt/2)
+                if (i > differnt/2 - 1  && i < this.SizeX - (differnt/2))
                 {
-                    this.Board[(this.SizeX - 1) / 2, i] = '@';
-                }
-                else
-                {
-                    this.Board[(this.SizeX - 1) / 2, i] = startingLongWord[k];
+                    this.Board[this.SizeX / 2, i] = startingLongWord[k];
                     k++;
                 }
             }
+            int startingPoint = startingLongWord.Length / 4;
+
+            int newWordsLength = this.SizeX / 2;
+
+            string topWord = WordListFactory.WordsWithLetterArray(WordListFactory.WordListDansk(newWordsLength, newWordsLength),startingLongWord[startingPoint]).First(x => x.First() == startingLongWord[startingPoint]);
+            string bottomWord = WordListFactory.WordsWithLetterArray(WordListFactory.WordListDansk(newWordsLength, newWordsLength), startingLongWord[startingLongWord.Length - startingPoint - 1]).First(x => x.Last() == startingLongWord[startingLongWord.Length - startingPoint - 1]);
+
+            FillIllegalSpace();
+
+            AddWordVertically(topWord, this.SizeX / 2, differnt);
+
+            AddWordVertically(bottomWord, this.SizeX / 2 - newWordsLength + 1, this.SizeY - differnt - 1);
+
+            RotateArrayClockwise();
         }
         private void AddBlankAtCircularSymmetry(int x, int y)
         {
             this.Board[this.SizeX - x, this.SizeY - y] += '@'; 
+        }
+        private void FillIllegalSpace()
+        {
+            for (int i = 0; i < this.SizeX; i++)
+            {
+                if (this.Board[i, 2] == '@')
+                {
+                    this.Board[i, 1] = '@';
+                    this.Board[i, 0] = '@';
+                }
+                if (this.Board[i, 1] == '@')
+                {
+                    this.Board[i, 0] = '@';
+                }
+                if (this.Board[i, this.SizeY - 3] == '@')
+                {
+                    this.Board[i, this.SizeY - 2] = '@';
+                    this.Board[i, this.SizeY - 1] = '@';
+                }
+                if (this.Board[i, this.SizeY - 2] == '@')
+                {
+                    this.Board[i, this.SizeY - 1] = '@';
+                }
+            }
+
+            for (int i = 0; i < this.SizeY; i++)
+            {
+                if (this.Board[1, i] == '@')
+                {
+                    this.Board[0, i] = '@';
+                }
+                if (this.Board[2, i] == '@')
+                {
+                    this.Board[1, i] = '@';
+                    this.Board[0, i] = '@';
+                }
+                if (this.Board[this.SizeX - 2, i] == '@')
+                {
+                    this.Board[this.SizeX - 1, i] = '@';
+                }
+                if (this.Board[this.SizeX - 3, i] == '@')
+                {
+                    this.Board[this.SizeX - 2, i] = '@';
+                    this.Board[this.SizeX - 1, i] = '@';
+                }
+            }
+        }
+        private void AddWordVertically(string word, int xStart, int yStart)
+        {
+            for (int i = 0; i < word.Length; i++)
+            {
+                this.Board[xStart + i, yStart] = word[i];
+            }
+        }
+        private void AddWordHorizontally(string word, int xStart, int yStart)
+        {
+            for (int i = 0; i < word.Length; i++)
+            {
+                this.Board[xStart, yStart + i] = word[i];
+            }
+        }
+        private void RotateArrayClockwise()
+        {
+            int width;
+            int height;
+            char[,] dst;
+
+            width = this.Board.GetUpperBound(0) + 1;
+            height = this.Board.GetUpperBound(1) + 1;
+            dst = new char[height, width];
+
+            for (int row = 0; row < height; row++)
+            {
+                for (int col = 0; col < width; col++)
+                {
+                    int newRow;
+                    int newCol;
+
+                    newRow = col;
+                    newCol = height - (row + 1);
+
+                    dst[newCol, newRow] = this.Board[col, row];
+                }
+            }
+
+            this.Board = dst;
         }
     }
 }
--- KrydsOgTværsKonsole/Program.cs
+++ KrydsOgTværsKonsole/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -11,6 +12,10 @@ namespace KrydsOgTværsKonsole
     public class Program
     {
         static void Main(string[] args)
+        {
+            testBoardFactory();
+        }
+        public static void testCrossword()
         {
             string[] arg = WordListFactory.WordListDansk(2, 6);
             CrosswordFactory crosswordFactory = new CrosswordFactory();
@@ -22,6 +27,23 @@ namespace KrydsOgTværsKonsole
                     Console.Write(crosswordFactory.CrosswordBoard[i, j]+"|");
                 }
                 
+                Console.WriteLine();
+            }
+            Console.WriteLine("Done!");
+            Console.ReadLine();
+        }
+        public static void testBoardFactory()
+        {
+            BoardFactory boardFactory = new BoardFactory(1,15);
+            boardFactory.StartBoardGeneration(WordListFactory.WordListDansk(11, 11).ElementAt(RNG.Roll(0, WordListFactory.WordListDansk(11, 11).Length)));
+
+            for (int i = 0; i < boardFactory.Board.GetLength(0); i++)
+            {
+                for (int j = 0; j < boardFactory.Board.GetLength(1); j++)
+                {
+                    Console.Write(boardFactory.Board[i, j] + "|");
+                }
+
                 Console.WriteLine();
             }
             Console.WriteLine("Done!");
